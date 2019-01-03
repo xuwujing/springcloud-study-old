03aa57e33ac32d6847dfdcdb931bb8d137ea434e
xuwujing <1060589146@qq.com>
2019-01-03 18:03:01 +0800

    springcloud配置中心示例代码提交

--- springcloud-config/springcloud-config-client/src/main/java/com/pancm/web/ClientController.java
+++ springcloud-config/springcloud-config-client/src/main/java/com/pancm/web/ClientController.java
@@ -9,10 +9,10 @@ import org.springframework.web.bind.annotation.RestController;
  * 
 * @Title: ClientController
 * @Description: springboot 接口测试
-* 首先启动 Application 程序，然后在浏览器输入http://localhost:9001//hello 
+* 首先启动 Application 程序，然后在浏览器输入http://localhost:9006//hello?name=pancm 
 * 即可查看信息
 * 在类中添加  @RestController, 默认类中的方法都会以json的格式返回
-* Version:1.0.0  
+* @Version:1.0.0  
 * @author pancm
 * @date 2018年1月3日
  */
